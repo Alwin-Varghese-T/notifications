09f2ffcc60296ff1ab489f0e50aae49d2bb1eed9
Alwin-Varghese-T <alwinvargheset@gmail.com>
2023-02-25 11:26:03 +0530

    first commit

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 from flask import Flask, render_template
-
-
+from flask_sqlalchemy import SQLAlchemy
+from test import RegisterForm
 app = Flask(__name__)
 
+# main page
+
 @app.route('/')
+
+@app.route('/pages-login.html')
+def login():
+    return render_template('pages-login.html')
+
 @app.route('/index.html')
 def home():
-    return render_template('index.html')
+
+    items = {'name' : 'Scheme name1', 'link' : 'https://email.gov.in/' },{'name' : 'Scheme name2', 'link' : 'https://email.gov.in/' },{'name' : 'Scheme name3' ,'link' : 'https://email.gov.in/' },{'name' : 'Scheme name4', 'link' : 'https://email.gov.in/' },{'name' : 'Scheme name5' ,'link' : 'https://email.gov.in/' }
+
+
+    return render_template('index.html', items=items)
 
 @app.route('/users-profile.html')
 def user():
     return render_template('users-profile.html')
-@app.route('/pages-login.html')
-def login():
-    return render_template('pages-login.html')
+
 @app.route('/pages-register.html')
 def register():
-    return render_template('pages-register.html')
+    form = RegisterForm()
+    return render_template('pages-register.html',form=form)
 
 if __name__ == '__main__':
     app.run()
+
+    
